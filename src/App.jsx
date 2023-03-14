78d0ca812c43b406e04f652f1efa007939c0f547
johnfinley <johnfinleybremerton@gmail.com>
2023-03-13 22:06:27 -0600

    start to build out layout and import components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,59 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+import Favorites from "./components/Favorites";
+import Icon from "./components/Icon";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [checked, setChecked] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (localStorage.theme === "dark") {
+      document.documentElement.classList.add("dark");
+      setChecked(true);
+      setLoading(false);
+    } else {
+      document.documentElement.classList.remove("dark");
+      setLoading(false);
+    }
+  });
+
+  const handleInput = () => {
+    if (localStorage.theme === "dark") {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+      setChecked(false);
+    } else {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+      setChecked(true);
+    }
+  };
 
   return (
-    <div className=''>
-      hello
+    <div className="font-roboto">
+      <h1 className="text-4xl font-black mb-3 dark:text-[#22daff]">
+        React Weather App
+      </h1>
+      <Favorites />
+      {loading ? null : (
+        <div className="flex justify-end items-center mt-3">
+          <div className="flex items-center gap-2">
+            <Icon checked={checked} />
+            <label class="relative inline-flex items-center cursor-pointer">
+              <input
+                type="checkbox"
+                class="sr-only peer"
+                onChange={handleInput}
+                checked={checked}
+              />
+              <div class="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-green-300 dark:peer-focus:ring-green-800 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-600"></div>
+            </label>
+          </div>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
