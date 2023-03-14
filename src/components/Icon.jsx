c21476d1cadd74f36a4c9be730cab00499b9fdd4
johnfinley <johnfinleybremerton@gmail.com>
2023-03-14 14:33:15 -0600

    update classes

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -3,9 +3,9 @@ import { FaSun, FaMoon } from "react-icons/fa";
 
 function Icon(props) {
   const icon = props.checked ? (
-    <FaMoon size={36} color="#F4FF8D" className="bg-slate-400 rounded-lg p-1" />
+    <FaMoon size={36} color="#F4FF8D" className="p-1" />
   ) : (
-    <FaSun size={36} color="#fce303" className="bg-slate-400 rounded-lg p-1" />
+    <FaSun size={36} color="#fce303" className="p-1" />
   );
 
   return <>{icon}</>;
