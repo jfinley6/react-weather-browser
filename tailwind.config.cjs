be2dcc19bd02be643230508a1a3dd9520b2230f2
johnfinley <johnfinleybremerton@gmail.com>
2023-03-13 22:05:31 -0600

    tailwind configuration

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        roboto: ['"Roboto Slab"'],
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
   darkMode: "class",
 };
