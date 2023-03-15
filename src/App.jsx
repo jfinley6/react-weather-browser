931cd508c4860334a3ad83f7b7bd217f78e74320
johnfinley <johnfinleybremerton@gmail.com>
2023-03-15 17:06:25 -0600

    refactor code out of App.jsx and into Icon.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,43 +19,16 @@ function App() {
     }
   });
 
-  const handleInput = () => {
-    if (localStorage.theme === "dark") {
-      document.documentElement.classList.remove("dark");
-      localStorage.setItem("theme", "light");
-      setChecked(false);
-    } else {
-      document.documentElement.classList.add("dark");
-      localStorage.setItem("theme", "dark");
-      setChecked(true);
-    }
-  };
+  if (componentLoading) return null;
 
   return (
     <div className="font-roboto">
       <h1 className="text-3xl md:text-4xl font-black mb-3 dark:text-[#22daff]">
         React Weather Browser
       </h1>
-      {componentLoading ? null : (
-        <>
-          <Search />
-          <Favorites />
-          <div className="flex justify-end items-center mt-3">
-            <div className="flex items-center gap-2 bg-slate-400 p-2 rounded-lg">
-              <Icon checked={checked} />
-              <label className="relative inline-flex items-center cursor-pointer">
-                <input
-                  type="checkbox"
-                  className="sr-only peer"
-                  onChange={handleInput}
-                  checked={checked}
-                />
-                <div className="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-green-300 dark:peer-focus:ring-green-800 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-600"></div>
-              </label>
-            </div>
-          </div>
-        </>
-      )}
+      <Search />
+      <Favorites />
+      <Icon checked={checked} setChecked={setChecked} />
     </div>
   );
 }
--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -8,7 +8,34 @@ function Icon(props) {
     <FaSun size={36} color="#fce303" className="p-1" />
   );
 
-  return <>{icon}</>;
+  const handleInput = () => {
+    if (localStorage.theme === "dark") {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+      props.setChecked(false);
+    } else {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+      props.setChecked(true);
+    }
+  };
+
+  return (
+    <div className="flex justify-end items-center mt-3">
+      <div className="flex items-center gap-2 bg-[rgb(249,250,251)] dark:bg-[rgb(55,65,81)] p-2 rounded-lg">
+        {icon}
+        <label className="relative inline-flex items-center cursor-pointer">
+          <input
+            type="checkbox"
+            className="sr-only peer"
+            onChange={handleInput}
+            checked={props.checked}
+          />
+          <div className="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-green-300 dark:peer-focus:ring-green-800 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-600"></div>
+        </label>
+      </div>
+    </div>
+  );
 }
 
 export default Icon;
