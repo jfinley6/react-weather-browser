f31ddc7d7c7327c6ff35670773a74977b208a65f
johnfinley <johnfinleybremerton@gmail.com>
2023-03-16 16:10:03 -0600

    moved local storage logic into seperate file

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,25 @@
 import { useState, useEffect } from "react";
+import { Context } from "./context/Store";
 import "./App.css";
 import Favorites from "./components/Favorites";
 import Icon from "./components/Icon";
 import Search from "./components/Search";
-import { Context } from "./context/Store";
+import { getFavoriteCities, getTheme } from "./functions/getLocalStorage";
 
 function App() {
   const [checked, setChecked] = useState(false);
   const [componentLoaded, setComponentLoaded] = useState(false);
+  const [favorites, setFavorites] = useState([]);
 
   useEffect(() => {
-    if (localStorage.theme === "dark") {
-      document.documentElement.classList.add("dark");
-      setChecked(true);
-      setComponentLoaded(true);
-    } else {
-      document.documentElement.classList.remove("dark");
-      setComponentLoaded(true);
-    }
-  });
+    getFavoriteCities(setFavorites)
+    getTheme(setChecked, setComponentLoaded)
+  },[]);
 
   if (!componentLoaded) return null;
 
   return (
-    <Context.Provider value={{ checked, setChecked }}>
+    <Context.Provider value={{ checked, setChecked, favorites }}>
       <div className="font-roboto">
         <h1 className="text-3xl md:text-5xl font-black mb-3 md:mb-6 dark:text-[#22daff]">
           React Weather Browser
