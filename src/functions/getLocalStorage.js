e49e9959efc004107b49091ea6bc3046594edc61
johnfinley <johnfinleybremerton@gmail.com>
2023-03-16 16:09:35 -0600

    added functions for initial local storage checks

---
+++ src/functions/getLocalStorage.js
@@ -0,0 +1,38 @@
+export const getFavoriteCities = (setFavorites) => {
+  const initialCities = [
+    {
+      city: "Denver",
+      state: "CO",
+    },
+    {
+      city: "New York",
+      state: "NY",
+    },
+    {
+      city: "Los Angeles",
+      state: "CA",
+    },
+  ];
+
+  if (localStorage.getItem("cities") === null) {
+    //   setCurrentState(initialCities[0].state);
+    //   setCurrentCity(initialCities[0].city);
+    setFavorites(initialCities);
+    localStorage.setItem("cities", JSON.stringify(initialCities));
+  } else {
+    setFavorites(JSON.parse(localStorage.getItem("cities")));
+    //   setCurrentCity(JSON.parse(localStorage.getItem("cities"))[0].city);
+    //   setCurrentState(JSON.parse(localStorage.getItem("cities"))[0].state);
+  }
+};
+
+export const getTheme = (setChecked, setComponentLoaded) => {
+  if (localStorage.theme === "dark") {
+    document.documentElement.classList.add("dark");
+    setChecked(true);
+    setComponentLoaded(true);
+  } else {
+    document.documentElement.classList.remove("dark");
+    setComponentLoaded(true);
+  }
+}
