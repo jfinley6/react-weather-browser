15529d2865559c11953aa238f0dcea02f624ba0d
johnfinley <johnfinleybremerton@gmail.com>
2023-03-16 16:12:43 -0600

    table rows now rendered based on favorites state

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Table } from "flowbite-react";
+import { Context } from "../context/Store";
 
 function Favorites() {
+  const { favorites } = useContext(Context);
+
+  const favoritesList = favorites.map((favorite) => {
+    return (
+      <Table.Row
+        key={`${favorite.city},${favorite.state}`}
+        className="bg-white dark:border-gray-700 dark:bg-gray-800 hover:bg-gray-200 hover:cursor-pointer transform 
+                                transition duration-500 hover:scale-[102%] rounded-lg dark:hover:bg-gray-600"
+      >
+        <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+          1
+        </Table.Cell>
+        <Table.Cell>{favorite.city}</Table.Cell>
+        <Table.Cell>{favorite.state}</Table.Cell>
+      </Table.Row>
+    );
+  });
+
   return (
-    <div className="w-full flex flex-col items-center mt-2">
+    <div className="w-full flex flex-col items-center mt-3">
       <h3 className="dark:text-[#22daff] mb-1">Favorites</h3>
       <div className="overflow-y-auto overflow-x-hidden max-h-[50vh] w-full md:w-3/4">
         <Table>
@@ -17,36 +36,7 @@ function Favorites() {
             </Table.HeadCell>
           </Table.Head>
           <Table.Body className="divide-y overflow-y-scroll">
-            <Table.Row
-              className="bg-white dark:border-gray-700 dark:bg-gray-800 hover:bg-gray-200 hover:cursor-pointer transform 
-                                transition duration-500 hover:scale-[102%] rounded-lg dark:hover:bg-gray-600"
-            >
-              <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                1
-              </Table.Cell>
-              <Table.Cell>Denver</Table.Cell>
-              <Table.Cell>CO</Table.Cell>
-            </Table.Row>
-            <Table.Row
-              className="bg-white dark:border-gray-700 dark:bg-gray-800 hover:bg-gray-200 hover:cursor-pointer transform 
-                                transition duration-500 hover:scale-[102%] rounded-lg dark:hover:bg-gray-600"
-            >
-              <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                1
-              </Table.Cell>
-              <Table.Cell>Petaluma</Table.Cell>
-              <Table.Cell>CA</Table.Cell>
-            </Table.Row>
-            <Table.Row
-              className="bg-white dark:border-gray-700 dark:bg-gray-800 hover:bg-gray-200 hover:cursor-pointer transform 
-                                transition duration-500 hover:scale-[102%] rounded-lg dark:hover:bg-gray-600"
-            >
-              <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                1
-              </Table.Cell>
-              <Table.Cell>Bremerton</Table.Cell>
-              <Table.Cell>WA</Table.Cell>
-            </Table.Row>
+            {favoritesList}
           </Table.Body>
         </Table>
       </div>
