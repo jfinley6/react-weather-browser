166af60320ba2f65f2fe90bd253d069f98603a99
johnfinley <johnfinleybremerton@gmail.com>
2023-03-16 14:34:32 -0600

    removed props from parent in favor of context values

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import { FaSun, FaMoon } from "react-icons/fa";
+import { Context } from "../context/Store";
 
-function Icon(props) {
-  const icon = props.checked ? (
+function Icon() {
+  const {checked, setChecked} = useContext(Context)
+
+  const icon = checked ? (
     <FaMoon size={36} color="#F4FF8D" className="p-1" />
   ) : (
     <FaSun size={36} color="#fce303" className="p-1" />
@@ -12,11 +15,11 @@ function Icon(props) {
     if (localStorage.theme === "dark") {
       document.documentElement.classList.remove("dark");
       localStorage.setItem("theme", "light");
-      props.setChecked(false);
+      setChecked(false);
     } else {
       document.documentElement.classList.add("dark");
       localStorage.setItem("theme", "dark");
-      props.setChecked(true);
+      setChecked(true);
     }
   };
 
@@ -29,7 +32,7 @@ function Icon(props) {
             type="checkbox"
             className="sr-only peer"
             onChange={handleInput}
-            checked={props.checked}
+            checked={checked}
           />
           <div className="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-[#22daff]"></div>
         </label>
