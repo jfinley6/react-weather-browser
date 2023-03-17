d1e04b961ec5a43e14d2aa8da244dbcd7d557d6e
johnfinley <johnfinleybremerton@gmail.com>
2023-03-17 12:44:00 -0600

    updated text size

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -25,7 +25,7 @@ const Search = () => {
         id="google-input"
         apiKey={import.meta.env.VITE_GOOGLE_AUTOCOMPLETE_KEY}
         placeholder="Search for Cities Across The United States..."
-        className="flex w-full p-2 pl-3 text-lg text-gray-900 border border-gray-300 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
+        className="flex w-full p-2 pl-3 text-sm md:text-lg text-gray-900 border border-gray-300 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
         onPlaceSelected={(place) => {
           console.log(place);
         }}
