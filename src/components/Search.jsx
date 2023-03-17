df66a5ab67f904048c30a88d84ca12f159a61cee
johnfinley <johnfinleybremerton@gmail.com>
2023-03-16 20:57:05 -0600

    add Clear button

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,23 +1,30 @@
 import React from "react";
 import Autocomplete from "react-google-autocomplete";
+import { Button } from "flowbite-react";
 
 function Search() {
-  const restrictions = {
-    country: "us",
-  };
-
-
+  
   return (
-    <div className="flex justify-center">
+    <div className="w-full md:w-1/2 left-[50%] translate-x-[-50%] relative">
       <Autocomplete
+        data-testid="autocomplete"
+        id="google-input"
         apiKey={import.meta.env.VITE_GOOGLE_AUTOCOMPLETE_KEY}
         placeholder="Search for Cities Across The United States..."
-        className="block w-full md:w-1/2 p-4 pl-3 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        className="flex w-full p-2 pl-3 text-lg text-gray-900 border border-gray-300 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
         onPlaceSelected={(place) => {
           console.log(place);
         }}
         options={{ componentRestrictions: { country: "us" } }}
       />
+      <Button
+        data-testid="clear-button"
+        size={"xs"}
+        style={{ margin: "auto" }}
+        className="absolute right-3 top-0 bottom-0"
+      >
+        Clear
+      </Button>
     </div>
   );
 }
