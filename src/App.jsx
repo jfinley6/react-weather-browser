813921734f136bd09ee0b117e83a7cbd203bf6f2
johnfinley <johnfinleybremerton@gmail.com>
2023-03-23 17:42:30 -0600

    move context values to variable

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,13 @@ import Icon from "./components/Icon";
 import Search from "./components/Search";
 import Weather from "./components/Weather";
 import { getFavoriteCities, getTheme } from "./functions/getLocalStorage";
+import Loading from "./components/Loading";
 
 const App = () => {
   const [checked, setChecked] = useState(false);
   const [componentLoaded, setComponentLoaded] = useState(false);
   const [weatherLoaded, setWeatherLoaded] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [favorites, setFavorites] = useState([]);
   const [weatherData, setWeatherData] = useState({
     city: "",
@@ -24,6 +26,16 @@ const App = () => {
     humidity: "",
     temp: "",
   });
+  const contextValues = {
+    checked,
+    setChecked,
+    favorites,
+    setFavorites,
+    weatherData,
+    setWeatherData,
+    setWeatherLoaded,
+    setLoading,
+  };
 
   useEffect(() => {
     getFavoriteCities(setFavorites);
@@ -33,21 +45,13 @@ const App = () => {
   if (!componentLoaded) return null;
 
   return (
-    <Context.Provider
-      value={{
-        checked,
-        setChecked,
-        favorites,
-        weatherData,
-        setWeatherData,
-        setWeatherLoaded
-      }}
-    >
+    <Context.Provider value={contextValues}>
       <div className="font-roboto">
         <h1 className="text-3xl md:text-5xl font-black mb-3 md:mb-6 dark:text-[#22daff]">
           React Weather Browser
         </h1>
         <Search />
+        {loading ? <Loading /> : null}
         {weatherLoaded ? <Weather /> : null}
         <Favorites />
         <Icon />
