b516426c77c62b060716a0aff629da5a332b174d
johnfinley <johnfinleybremerton@gmail.com>
2023-03-26 16:18:02 -0600

    replace loading text with spinner

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { SpinnerCircularFixed } from "spinners-react";
 
 function Loading() {
   return (
-    <div>Loading...</div>
-  )
+    <div className='w-full flex justify-center mt-4'>
+      <SpinnerCircularFixed
+        size={50}
+        thickness={100}
+        speed={100}
+        color="#36ad47"
+        secondaryColor="rgba(0, 0, 0, 0.44)"
+      />
+    </div>
+  );
 }
 
 export default Loading
